3fb3da4a8ecdb96288df4b0268d76463ac384bcc
Charles <didi831313@gmail.com>
2024-05-25 22:12:57 +0200

    Ca avance

--- app/src/main/java/com/example/dobrazil/Constants.kt
+++ app/src/main/java/com/example/dobrazil/Constants.kt
@@ -1,8 +1,16 @@
 package com.example.dobrazil
 
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+
 const val IvoryColor = 0xFFDFDFCB
 const val GreenVariantColor = 0xFF05944F
 const val IvoryBorderColor  = 0xFFb3b3a2
 const val InputFieldGreen = 0xFF195036
 const val InputFieldBrown = 0xFF5B4527
 const val GreenVariantStrongColor = 0xFF07542f
+
+val poppins = FontFamily(
+    Font(R.font.poppins_medium),
+    Font(R.font.poppins_semi_bold)
+)
--- app/src/main/java/com/example/dobrazil/Profil.kt
+++ app/src/main/java/com/example/dobrazil/Profil.kt
@@ -1,7 +1,6 @@
 package com.example.dobrazil
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,6 +18,8 @@ import com.example.dobrazil.ui.theme.DoBrazilTheme
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
@@ -26,6 +27,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 
 /**
@@ -34,6 +38,7 @@ import androidx.compose.foundation.layout.size
 @Composable
 fun Profil() {
     val scrollState = rememberScrollState()
+    val currentlySoon = remember { mutableStateOf(true) }
 
     Column( // Column that fill the entire screen
         modifier = Modifier
@@ -56,23 +61,29 @@ fun Profil() {
                     contentDescription = "Profil picture",
                     contentScale = ContentScale.Crop, // Crop the image to fit the circle
                     modifier = Modifier
-                        .clip(CircleShape)
-                        .border(4.dp, Color(GreenVariantStrongColor), CircleShape) // Add a border to the image
-                        .size(140.dp)
+                        .clip(CircleShape) // Clip the image to a circle
+                        .border(
+                            4.dp,
+                            Color(GreenVariantStrongColor),
+                            CircleShape
+                        ) // Add a border to the image
+                        .size(140.dp) // Size of the image
                 )
             }
 
             Column (
-                horizontalAlignment = Alignment.CenterHorizontally,
+                horizontalAlignment = Alignment.CenterHorizontally, // Align the column to the center
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .fillMaxWidth() // Fill the entire width of the screen
                     .padding(8.dp),
             ){
                 Text("Nom de l'utilisateur") // Username of the user
+                /* TODO Remplacer par le nom de l'utilisateur */
 
                 Spacer(modifier = Modifier.padding(10.dp))
 
-                Text("Email de l'utilisateur") // Firstname of the user
+                Text("Email de l'utilisateur") // Email of the user
+                /* TODO Remplacer par l'email de l'utilisateur */
             }
 
         }
@@ -82,10 +93,18 @@ fun Profil() {
         /*------------------------------- Categorie container -------------------------------*/
         Row (
             modifier = Modifier
-                .fillMaxWidth()
-                .background(Color(GreenVariantColor))
+                .fillMaxWidth() // Fill the entire width of the screen
+                .background(Color(GreenVariantColor)), // Background color of the row
+            horizontalArrangement = Arrangement.SpaceEvenly // Space the element evenly
+
         ){
-            Text("Catégorie")
+            // Button to show the event that are soon
+            CategorieButton(onClick = {currentlySoon.value = true}, R.drawable.categorie_soon, Modifier.weight(1f))
+
+            // Button to show the event that are currently
+            CategorieButton(onClick = {currentlySoon.value = false}, R.drawable.categorie_party, Modifier.weight(1f))
+
+            // Both of them are weighted to take the same space
         }
 
         BottomBorder(width = 6.dp, color = Color(IvoryBorderColor))
@@ -97,11 +116,42 @@ fun Profil() {
                 .background(Color(IvoryColor))
                 .verticalScroll(scrollState),
         ){
-            Text("Les trucs de la catégorie")
+            if (currentlySoon.value) {
+                Text("Bientôt disponible")
+            } else {
+                Text("Liste des événements")
+            }
 
         }
     }
 }
+
+/**
+ * @brief Composable that modelise categorie button
+ * @param categorieIcon : Int that represent the icon of the categorie
+ * @param onClick : Function that represent the action when the button is clicked
+ * @param modifier : Modifier of the button
+ */
+@Composable
+fun CategorieButton(onClick: () -> Unit, categorieIcon: Int, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(categorieIcon), // Icon of the categorie
+        contentDescription = "", // Description of the image
+        modifier = modifier // Modifier of the image
+            .clickable { onClick() } // Add a clickable action to the image
+            .border(4.dp, Color(GreenVariantStrongColor)) // Add a border to the image
+    )
+}
+
+/**
+ * @brief Composable that modelise an event
+ * @param eventId : Int that represent the id of the event
+ */
+@Composable
+fun Event(eventId: Int) {
+
+}
+
 
 /**
  * @brief Composable that allow to preview the profil page
