b21bb083a5e39bd4e6895a8a068fde1488bd0da9
Charles <didi831313@gmail.com>
2024-05-26 00:14:13 +0200

    Profil.kt frontend fini

--- app/src/main/java/com/example/dobrazil/Profil.kt
+++ app/src/main/java/com/example/dobrazil/Profil.kt
@@ -21,16 +21,29 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 /**
  * @brief Composable that allow to the profil page
@@ -38,7 +51,7 @@ import androidx.compose.runtime.remember
 @Composable
 fun Profil() {
     val scrollState = rememberScrollState()
-    val currentlySoon = remember { mutableStateOf(true) }
+    val page = remember { mutableStateOf(0) }
 
     Column( // Column that fill the entire screen
         modifier = Modifier
@@ -77,6 +90,17 @@ fun Profil() {
                     .fillMaxWidth() // Fill the entire width of the screen
                     .padding(8.dp),
             ){
+
+                Icon (
+                    Icons.Default.Home,
+                    contentDescription = "Home icon",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clickable { /* TODO allow to go to the menu*/ }
+                )
+
+                Spacer(modifier = Modifier.padding(10.dp))
+
                 Text("Nom de l'utilisateur") // Username of the user
                 /* TODO Remplacer par le nom de l'utilisateur */
 
@@ -85,6 +109,7 @@ fun Profil() {
                 Text("Email de l'utilisateur") // Email of the user
                 /* TODO Remplacer par l'email de l'utilisateur */
             }
+
 
         }
 
@@ -99,12 +124,15 @@ fun Profil() {
 
         ){
             // Button to show the event that are soon
-            CategorieButton(onClick = {currentlySoon.value = true}, R.drawable.categorie_soon, Modifier.weight(1f))
+            CategorieButton(onClick = {page.value = 0}, R.drawable.categorie_soon, Modifier.weight(1f))
 
             // Button to show the event that are currently
-            CategorieButton(onClick = {currentlySoon.value = false}, R.drawable.categorie_party, Modifier.weight(1f))
+            CategorieButton(onClick = {page.value = 1}, R.drawable.categorie_party, Modifier.weight(1f))
 
-            // Both of them are weighted to take the same space
+            // Button to show the your favorite people
+            CategorieButton(onClick = {page.value = 2}, R.drawable.categorie_favorite, Modifier.weight(1f))
+
+            // they are all weighted to take the same space
         }
 
         BottomBorder(width = 6.dp, color = Color(IvoryBorderColor))
@@ -112,14 +140,20 @@ fun Profil() {
         /*------------------------------- List of the category -------------------------------*/
         Column (
             modifier = Modifier
-                .fillMaxSize()
-                .background(Color(IvoryColor))
-                .verticalScroll(scrollState),
+                .fillMaxSize() // Fill the entire size of the screen remaining
+                .background(Color(IvoryColor)) // Background color of the column
+                .verticalScroll(scrollState) // Add a scroll to the column to see all the element
+                .padding(8.dp),
         ){
-            if (currentlySoon.value) {
-                Text("Bientôt disponible")
-            } else {
-                Text("Liste des événements")
+            if (page.value == 0) { // If the user want to see the event that are made by him / coming soon
+                for (i in 0..10) {
+                    People()
+                    Spacer(modifier = Modifier.padding(8.dp))
+                }
+            } else if (page.value == 1) { // If the user want to see the event that are made by him / currently
+                Event()
+            } else { // If the user want to see the favorite people
+                People()
             }
 
         }
@@ -148,8 +182,161 @@ fun CategorieButton(onClick: () -> Unit, categorieIcon: Int, modifier: Modifier 
  * @param eventId : Int that represent the id of the event
  */
 @Composable
-fun Event(eventId: Int) {
+fun Event(eventId: Int = 0) {
+    Row ( // Row that contains the element of the event
+        modifier = Modifier
+            .border(4.dp, Color(GreenVariantStrongColor)) // Add a border to the row
+            .background(Color(GreenVariantColor)) // Background color of the row
+            .fillMaxWidth() // Fill the entire width of the screen
+            .padding(8.dp)  // Add a padding to the row
+            .height(200.dp) // Height of the row
+    ){
+        Column ( // Column that contains the element the picture of the event
+            modifier = Modifier
+                .fillMaxWidth(0.35f)
+                .fillMaxHeight()
+                .padding(4.dp)
+        ){
+            Image(
+                painter = painterResource(R.drawable.event_default_pfp), // Image of the event
+                /* TODO Remplacer par l'image de l'événement */
+                contentDescription = "",
+                contentScale = ContentScale.FillBounds, // Fill the entire space of the image
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp)) // Clip the image to a rounded corner
+                    .border(2.dp, Color(GreenVariantStrongColor), RoundedCornerShape(8.dp)) // Add a border to the image
+            )
 
+        }
+
+        Column ( // Column that contains the element descripting the event
+            verticalArrangement = Arrangement.SpaceEvenly, // Space the element inside evenly
+            modifier = Modifier
+                .fillMaxSize() // Fill the entire size of the column
+                .fillMaxHeight()
+        ){
+            Row( // Row that contains the name, location and number of person of the event
+                horizontalArrangement = Arrangement.SpaceEvenly, // Space the element inside evenly
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.15f)
+            ){
+                Text("XXX") // Name of the event
+                /* TODO Remplacer par le nom de l'événement */
+                Text("Lieu") // Location of the event
+                /* TODO Remplacer par le lieu de l'événement */
+                Text("X pers.") // Number of person that are going to the event
+                /* TODO Remplacer par le nombre de personne qui vont à l'événement */
+                Box {
+                    DropDownMenu()
+                }
+            }
+
+            Row ( // Row that contains the description of the event
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.85f)
+                    .border(1.dp, Color(GreenVariantStrongColor))
+            ){
+                Text("Description") // Description of the event
+                /* TODO Remplacer par la description de l'événement */
+            }
+
+            Row ( // Row that contains the date of the event
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ){
+                Text("Date de début") // Date of the beginning of the event
+                /* TODO Remplacer par la date de début de l'événement */
+                Text("Date de fin") // Date of the end of the event
+                /* TODO Remplacer par la date de fin de l'événement */
+            }
+        }
+    }
+
+}
+
+/**
+ * @brief Composable that moodelise people that you put in favorite
+ * @param peopleId : Int that represent the id of the people
+ */
+@Composable
+fun People(peopleId: Int = 0) {
+    Row ( // Row that contains the element of the people
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .border(4.dp, Color(GreenVariantStrongColor)) // Add a border to the row
+            .background(Color(GreenVariantColor)) // Background color of the row
+            .fillMaxWidth() // Fill the entire width of the screen
+            .padding(8.dp)  // Add a padding to the row
+    ){
+        Image(
+                painter = painterResource(R.drawable.default_pfp_do_brazil), // Image of the people
+                /* TODO Remplacer par l'image de la personne */
+                contentDescription = "",
+                contentScale = ContentScale.FillBounds, // Fill the entire space of the image
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp)) // Clip the image to a rounded corner
+                    .border(2.dp, Color(GreenVariantStrongColor), RoundedCornerShape(8.dp)) // Add a border to the image
+                    .size(70.dp)
+        )
+
+        Text("Nom de la personne") // Name of the people
+        /* TODO Remplacer par le nom de la personne */
+
+        Box {
+            DropDownMenu()
+        }
+
+    }
+}
+
+
+/**
+ * @brief Composable that modelise a DropDownMenu
+ * @param isCreator : Boolean that represent if the user is the creator of the event
+ * @param isEvent : Boolean that represent if the dropDownMenu is for an event
+ */
+@Composable
+fun DropDownMenu(isCreator : Boolean = false, isEvent : Boolean = true) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box { // Box that contains the DropDownMenu
+        IconButton(onClick = { expanded = !expanded }) { // Button that show the DropDownMenu
+            Icon( // Icon of the button
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = "More"
+            )
+        }
+
+        DropdownMenu( // DropDownMenu that is shown when the button is clicked
+            expanded = expanded,
+            onDismissRequest = { expanded = false } // Dismiss the DropDownMenu when the user click outside of it
+        ) {
+            if (isEvent) { // If the DropDownMenu is for an event
+                if (isCreator) {  // If the user is the creator of the event
+                    DropdownMenuItem( // Show the option to manage the event
+                        text = { Text("Gerer") },
+                        onClick = { /* TODO */ }
+                    )
+                }
+                DropdownMenuItem( // Show the option to delete/quit the event
+                    text = { Text("Supprimer/Quitter") },
+                    onClick = { /* TODO */ }
+                )
+            } else {
+                DropdownMenuItem( // Show the option to delete the people from the favorite
+                    text = { Text("Supprimer") },
+                    onClick = { /* TODO */ }
+                )
+            }
+
+        }
+    }
 }
 
 
