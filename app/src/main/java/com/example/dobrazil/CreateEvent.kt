6a41e02f1a67538ce989dad4070a5fe119dba25c
Charles <didi831313@gmail.com>
2024-05-26 05:13:53 +0200

    J'enleve des imports inutiles

--- app/src/main/java/com/example/dobrazil/CreateEvent.kt
+++ app/src/main/java/com/example/dobrazil/CreateEvent.kt
@@ -20,13 +20,10 @@ import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.Title
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
